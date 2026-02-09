eba59c73b9833e00a0d356c4da239652b8d14562
Lionell Briones <llenoil@gmail.com>
2026-02-09 18:22:41 +0800

    move desktopInstallLinks and mobileInstallLinks inside bodysheets

--- packages/modal/src/ui/containers/Root/Root.tsx
+++ packages/modal/src/ui/containers/Root/Root.tsx
@@ -1,18 +1,14 @@
 import { WALLET_CONNECTORS, type WalletRegistryItem } from "@web3auth/no-modal";
-import { JSX, useCallback, useMemo, useState } from "react";
-import { useTranslation } from "react-i18next";
+import { useCallback, useMemo, useState } from "react";
 
 import Footer from "../../components/Footer/Footer";
-import Image from "../../components/Image";
 import Loader from "../../components/Loader";
 import Toast from "../../components/Toast";
 import { DEFAULT_METAMASK_WALLET_REGISTRY_ITEM, PAGES } from "../../constants";
 import { useModalState } from "../../context/ModalStateContext";
 import { RootProvider, useRoot } from "../../context/RootContext";
 import { useWidget } from "../../context/WidgetContext";
-import { ExternalButton, mobileOs, MODAL_STATUS } from "../../interfaces";
-import i18n from "../../localeImport";
-import { cn, getBrowserExtensionUrl, getBrowserName, getIcons, getMobileInstallLink, getOsName } from "../../utils";
+import { ExternalButton, MODAL_STATUS } from "../../interfaces";
 import ConnectWallet from "../ConnectWallet";
 import Login from "../Login";
 import { RootProps } from "./Root.type";
@@ -29,10 +25,9 @@ function RootContent(props: RootProps) {
   } = props;
 
   const { modalState, setModalState, shouldShowLoginPage, showPasswordLessInput, areSocialLoginsVisible } = useModalState();
-  const { isDark, appLogo, deviceDetails, uiConfig } = useWidget();
-  const { buttonRadiusType, chainNamespaces, walletRegistry, privacyPolicy, tncLink, displayInstalledExternalWallets, hideSuccessScreen } = uiConfig;
+  const { appLogo, deviceDetails, uiConfig } = useWidget();
+  const { chainNamespaces, walletRegistry, privacyPolicy, tncLink, displayInstalledExternalWallets, hideSuccessScreen } = uiConfig;
 
-  const [t] = useTranslation(undefined, { i18n });
   const { bodyState } = useRoot();
 
   const [isSocialLoginsExpanded, setIsSocialLoginsExpanded] = useState(false);
@@ -53,118 +48,6 @@ function RootContent(props: RootProps) {
     });
     if (handleExternalWalletBtnClick) handleExternalWalletBtnClick(flag);
   };
-
-  // Wallet Details
-  const mobileInstallLinks = useMemo<JSX.Element[]>(() => {
-    if (deviceDetails.platform === "desktop") return [];
-    const installConfig = bodyState.installLinks?.wallet?.walletRegistryItem?.app || {};
-    const installLinks = Object.keys(installConfig).reduce((acc, osKey) => {
-      if (!["android", "ios"].includes(osKey)) return acc;
-      const appId = installConfig[osKey as mobileOs];
-      if (!appId) return acc;
-      const appUrl = getMobileInstallLink(osKey as mobileOs, appId);
-      if (!appUrl) return acc;
-      const logoLight = `${osKey}-light`;
-      const logoDark = `${osKey}-dark`;
-      acc.push(
-        <li key={appUrl} className="w3a--w-full">
-          <a href={appUrl} rel="noopener noreferrer" target="_blank">
-            <button
-              type="button"
-              className={cn(
-                "w3a--group w3a--relative w3a--overflow-hidden w3a--h-11 w3a--flex w3a--w-full w3a--items-center w3a--justify-start w3a--gap-x-2 w3a--border w3a--border-app-gray-200 w3a--bg-app-gray-50 w3a--px-5 w3a--py-2.5 hover:w3a--translate-y-[0.5px] hover:w3a--border-app-gray-50 dark:w3a--border-app-gray-500 dark:w3a--bg-app-gray-800 dark:hover:w3a--border-app-gray-800",
-                {
-                  "w3a--rounded-full": buttonRadiusType === "pill",
-                  "w3a--rounded-lg": buttonRadiusType === "rounded",
-                  "w3a--rounded-none": buttonRadiusType === "square",
-                }
-              )}
-            >
-              <Image
-                imageId={logoLight}
-                darkImageId={logoDark}
-                hoverImageId={logoLight}
-                darkHoverImageId={logoDark}
-                height="28"
-                width="28"
-                isButton
-              />
-              <span className="w3a--text-sm w3a--font-medium w3a--text-app-gray-900 dark:w3a--text-app-white">
-                {t("modal.external.install-mobile-app", { os: getOsName(osKey as mobileOs) })}
-              </span>
-              <img
-                id="install-links-arrow"
-                className="w3a--absolute w3a--right-4 w3a--top-1/2 -w3a--translate-x-6 -w3a--translate-y-1/2 w3a--opacity-0 w3a--transition-all w3a--duration-300
-          group-hover:w3a--translate-x-0 group-hover:w3a--opacity-100"
-                src={getIcons(isDark ? "chevron-right-dark" : "chevron-right-light")}
-                alt="arrow"
-              />
-            </button>
-          </a>
-        </li>
-      );
-      return acc;
-    }, []);
-    return installLinks;
-  }, [bodyState.installLinks?.wallet?.walletRegistryItem?.app, deviceDetails.platform, isDark, t, buttonRadiusType]);
-
-  const desktopInstallLinks = useMemo<JSX.Element[]>(() => {
-    if (deviceDetails.platform !== "desktop") return [];
-    // if browser is brave, use chrome extension
-    const browserType = deviceDetails.browser === "brave" ? "chrome" : deviceDetails.browser;
-
-    const browserExtensionConfig = bodyState.installLinks?.wallet?.walletRegistryItem?.app || {};
-    const extensionForCurrentBrowser =
-      browserExtensionConfig.browser && browserExtensionConfig.browser.includes(browserType) ? browserExtensionConfig.browser : undefined;
-    const browserExtensionId = browserExtensionConfig[browserType as keyof typeof browserExtensionConfig] || extensionForCurrentBrowser;
-    const browserExtensionUrl = browserExtensionId ? getBrowserExtensionUrl(browserType, browserExtensionId) : null;
-    const installLink = browserExtensionUrl ? (
-      <li key={browserExtensionUrl}>
-        <a href={browserExtensionUrl} rel="noopener noreferrer" target="_blank">
-          <button
-            type="button"
-            className={cn(
-              "w3a--group w3a--relative w3a--overflow-hidden w3a--h-11 w3a--flex w3a--w-full w3a--items-center w3a--justify-start w3a--gap-x-2 w3a--border w3a--border-app-gray-200 w3a--bg-app-gray-50 w3a--px-5 w3a--py-2.5 hover:w3a--translate-y-[0.5px] hover:w3a--border-app-gray-50 dark:w3a--border-app-gray-500 dark:w3a--bg-app-gray-800 dark:hover:w3a--border-app-gray-800",
-              {
-                "w3a--rounded-full": buttonRadiusType === "pill",
-                "w3a--rounded-lg": buttonRadiusType === "rounded",
-                "w3a--rounded-none": buttonRadiusType === "square",
-              }
-            )}
-          >
-            <Image
-              imageId={deviceDetails.browser}
-              darkImageId={deviceDetails.browser}
-              hoverImageId={deviceDetails.browser}
-              darkHoverImageId={deviceDetails.browser}
-              height="30"
-              width="30"
-              isButton
-            />
-            <span className="w3a--text-sm w3a--font-medium w3a--text-app-gray-900 dark:w3a--text-app-white">
-              {t("modal.external.install-browser-extension", { browser: getBrowserName(deviceDetails.browser) })}
-            </span>
-            <img
-              id="install-links-arrow"
-              className="w3a--absolute w3a--right-4 w3a--top-1/2 -w3a--translate-x-6 -w3a--translate-y-1/2 w3a--opacity-0 w3a--transition-all w3a--duration-300
-          group-hover:w3a--translate-x-0 group-hover:w3a--opacity-100"
-              src={getIcons(isDark ? "chevron-right-dark" : "chevron-right-light")}
-              alt="arrow"
-            />
-          </button>
-        </a>
-      </li>
-    ) : null;
-    return [installLink, ...mobileInstallLinks];
-  }, [
-    bodyState.installLinks?.wallet?.walletRegistryItem?.app,
-    deviceDetails.browser,
-    deviceDetails.platform,
-    isDark,
-    mobileInstallLinks,
-    buttonRadiusType,
-    t,
-  ]);
 
   // External Wallets
   const config = useMemo(() => modalState.externalWalletsConfig, [modalState.externalWalletsConfig]);
@@ -426,11 +309,7 @@ function RootContent(props: RootProps) {
           {/* Footer */}
           <Footer privacyPolicy={privacyPolicy} termsOfService={tncLink} />
 
-          <RootBodySheets
-            preHandleExternalWalletClick={preHandleExternalWalletClick}
-            desktopInstallLinks={desktopInstallLinks}
-            mobileInstallLinks={mobileInstallLinks}
-          />
+          <RootBodySheets preHandleExternalWalletClick={preHandleExternalWalletClick} />
         </div>
       </div>
       <Toast />
